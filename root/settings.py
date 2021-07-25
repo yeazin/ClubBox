6087d13b6f662d8194555590be98a2955aaee677
yeazin <farsin310yeariha9701@gmail.com>
2021-07-25 22:49:17 +0600

    updat'e

--- root/settings.py
+++ root/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os 
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
