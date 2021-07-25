e85d732f10fb45b1022fdd46a9954fc2549ba8ca
yeazin <farsin310yeariha9701@gmail.com>
2021-07-25 23:26:42 +0600

    add

--- root/settings.py
+++ root/settings.py
@@ -121,7 +121,7 @@ AUTH_USER_MODEL = 'club.User'
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR,'media')
