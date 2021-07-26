d1a14aa7b5ae9a0d8d1f8076e758ac899188c420
yeazin <farsin310yeariha9701@gmail.com>
2021-07-26 08:30:24 +0600

    added static

---
+++ staticfiles/js/custom.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'invoice.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        });
+    };
+    const $btnPrint = document.querySelector("#btnPrint");
+    $btnPrint.addEventListener("click", () => {
+    window.print();
+    });
